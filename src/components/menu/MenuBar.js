2a2c50ba213200b140467cbb6a8372a59b76cf90
knwch <knwch@harmoinc.local>
2020-03-05 17:19:05 +0700

    add pages

---
+++ src/components/menu/MenuBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class MenuBar extends Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+
+}
