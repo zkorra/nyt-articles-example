70e88f6f813ab3a41a9a758a84b78d46ed73cc7c
zkorra <korrawich.dev@outlook.com>
2020-03-05 18:10:51 +0700

    fetch articles

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react'
+import { Card, Grid, Image, Icon, Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 export default class Home extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            articles: []
+        }
+    }
+
+    componentDidMount = () => {
+        this.fetchArticles();
+    }
+
+    fetchArticles = () => {
+        let currentState = this.state.articles
+        fetch(`https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=BNo7OVOfOzlQaP1AGGoDA4NScDqcWurb`)
+            .then((res) => res.json())
+            .then((json) => this.setState({articles: currentState.concat(json.results)}))
+    }
+
     render() {
-        return(
-            <div>
-                
-            </div>
+        return (
+            <Grid textAlign='center'>
+                {console.log(this.state.articles)}
+                <Grid.Column mobile={16} tablet={7} computer={7}>
+                    {this.state.articles
+                        .map((article, index) => {
+                            return (
+                                <div key={index}>
+                                    <Link to={`/booking/${article.slug_name}`}>
+                                        <Card link fluid>
+                                            {/* <Image src={article.pictureurl} wrapped /> */}
+                                            <Card.Content>
+                                                <Card.Header as='h3' textAlign='left'>
+                                                    {article.title}
+                                                </Card.Header>
+                                                <Card.Description textAlign='left'>
+                                                    {article.abstract}
+                                                </Card.Description>
+                                            </Card.Content>
+                                        </Card>
+                                    </Link>
+                                </div>
+                            )
+                        })}
+                </Grid.Column>
+            </Grid>
         );
     }
 
