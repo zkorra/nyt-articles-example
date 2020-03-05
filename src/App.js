6ad16279beab7459bee0a06d9dcbbd856ecc7246
zkorra <korrawich.dev@outlook.com>
2020-03-05 17:19:05 +0700

    add pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import './App.css';
+import Home from './components/pages/Home';
+import Article from './components/pages/Article';
+import MenuBar from './components/menu/MenuBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <MenuBar />
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/article/:id" component={Article} />
+          <Route exact path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
