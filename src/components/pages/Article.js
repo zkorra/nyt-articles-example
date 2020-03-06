cf66a7d8da55e8f0cc3d2e59324e609b9f7f0579
zkorra <korrawich.dev@outlook.com>
2020-03-06 11:26:23 +0700

    store data in localstorage

--- src/components/pages/Article.js
+++ src/components/pages/Article.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
 
 export default class Article extends Component {
-    render() {
-        const { data } = this.props.location
 
-        return(
+    constructor(props) {
+        super(props)
+        this.state = {
+            article: []
+        }
+    }
+
+    componentDidMount() {
+        const { data } = this.props.location
+        let routeState
+        if (data) {
+            localStorage.setItem('routeState', JSON.stringify(data))
+            routeState = data
+        } else {
+            routeState = localStorage.getItem('routeState')
+            if (routeState) routeState = JSON.parse(routeState)
+        }
+
+        this.setState({
+            article: routeState
+        })
+        
+    }
+
+    render() {
+        return (
             <div>
-                {console.log(data)}
+                {console.log(this.state.article)}
             </div>
         );
     }
